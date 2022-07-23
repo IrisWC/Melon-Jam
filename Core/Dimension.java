e3699d0ee3860acdd2696e4ea787ef8d8cf4d756
Iris Chou <iris.w.chou@gmail.com>
2022-07-23 12:41:02 -0700

    Update Dimension.java

--- Core/Dimension.java
+++ Core/Dimension.java
@@ -23,6 +23,12 @@ public class Dimension {
 					new Platform(444, 675, 200, 50, true), new Platform(688, 550, 200, 50, true), new Platform(931, 425, 200, 50, true)};
 			break;
 		case 2:
+			background = new ImageIcon("img\\Orange.png").getImage();
+			orb = new ImageIcon("img\\Orange Orb.png").getImage();
+			orbx = 1300;
+			orby = 200;
+			platforms = new Platform[] {new Platform(1175, 300, 300, 50, true), new Platform(100, 800, 300, 50, true),
+					new Platform(444, 675, 200, 50, true), new Platform(688, 550, 200, 50, true), new Platform(931, 425, 200, 50, true)};
 			break;
 		case 3:
 			break;
