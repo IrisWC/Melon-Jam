5c8ad1a275084663cad904dacb5e4cdfb9342b5c
Iris Chou <iris.w.chou@gmail.com>
2022-07-22 19:02:25 -0700

    Update Main.java

--- Core/Main.java
+++ Core/Main.java
@@ -1,5 +1,12 @@
 package Core;
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main {
+	public static void main(String args[]) {
+		JFrame window = new JFrame();
+		window.setSize(1600, 900);
+	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    window.setVisible(true);
+	}
 }
