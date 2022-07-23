2f9dced4fc5b6b30fb2e854442f777545c6725ca
Iris Chou <iris.w.chou@gmail.com>
2022-07-22 19:03:57 -0700

    Update Main.java

--- Core/Main.java
+++ Core/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		JFrame window = new JFrame();
 		window.setSize(1600, 900);
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 	    window.setVisible(true);
 	}
 }
