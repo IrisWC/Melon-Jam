bb4e5672bcb514ceeb3260b01f8ff1bd36b47391
Iris Chou <iris.w.chou@gmail.com>
2022-07-24 14:43:36 -0700

    Update game window icon

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -1,10 +1,15 @@
 package Core;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String args[]) {
 		JFrame window = new JFrame("Space Alchemy");
+		
+		ImageIcon img = new ImageIcon("Blue Orb.png");
+		window.setIconImage(img.getImage());
+		
 		window.setSize(1600, 900);
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
